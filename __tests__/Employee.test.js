01e0a3ef78dbc34a9a7cbff8131507b4a62a74bf
Will G <wgrout87@gmail.com>
2022-05-20 12:22:40 -0600

    Employee methods

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,4 +6,28 @@ test("Create an employee object", () => {
     expect(employee.name).toBe("Will");
     expect(employee.id).toBe(122345);
     expect(employee.email).toBe("email@gmail.com");
-})
+});
+
+test("Gets employee's name", () => {
+    const employee = new Employee("Will", 122345, "email@gmail.com");
+
+    expect(employee.getName()).toBe("Will");
+});
+
+test("Gets employee's ID", () => {
+    const employee = new Employee("Will", 122345, "email@gmail.com");
+
+    expect(employee.getId()).toBe(122345);
+});
+
+test("Gets employee's email", () => {
+    const employee = new Employee("Will", 122345, "email@gmail.com");
+
+    expect(employee.getEmail()).toBe("email@gmail.com");
+});
+
+test("Gets employee's email", () => {
+    const employee = new Employee("Will", 122345, "email@gmail.com");
+
+    expect(employee.getRole()).toBe("Employee");
+});
