2fa3f18933a90a9240327ace722ab858091c8014
Will G <wgrout87@gmail.com>
2022-05-22 15:55:11 -0600

    updated with DRY principles

--- src/page-template.js
+++ src/page-template.js
@@ -1,55 +1,36 @@
 const generateCard = employee => {
+    let roleSpecificInfo = ``;
+    let cardOutput = `
+    <div class="card col-12 col-md-6 col-lg-4 m-3 px-0 shadow-lg" style="max-width: 20rem;">
+        <div class="card-header text-white bg-primary">
+            <p class="fs-4">${employee.name}</p>`
     if (employee.getRole() === "Manager") {
-        return `
-        <div class="card col-12 col-md-6 col-lg-4 m-3 px-0 shadow-lg" style="max-width: 18rem;">
-            <div class="card-header text-white bg-primary">
-                <p>${employee.name}</p>
-                <p><i class="fa-solid fa-mug-hot"></i> ${employee.getRole()}</p>
-            </div>
-            <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${employee.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                    <li class="list-group-item">Office Number: ${employee.officeNumber}</li>
-                </ul>
-            </div>
-        </div>`
-    }
-
+        cardOutput += `
+            <p class="fs-5"><i class="fa-solid fa-mug-hot"></i> ${employee.getRole()}</p>`;
+        roleSpecificInfo = `<li class="list-group-item">Office Number: ${employee.officeNumber}</li>`;
+    };
     if (employee.getRole() === "Engineer") {
-        return `
-        <div class="card col-12 col-md-6 col-lg-4 m-3 px-0 shadow-lg" style="max-width: 18rem;">
-            <div class="card-header text-white bg-primary">
-                <p>${employee.name}</p>
-                <p><i class="fa-solid fa-glasses"></i> ${employee.getRole()}</p>
-            </div>
-            <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${employee.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                    <li class="list-group-item">GitHub: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a></li>
-                </ul>
-            </div>
-        </div>`
-    }
-
+        cardOutput += `
+            <p class="fs-5"><i class="fa-solid fa-glasses"></i> ${employee.getRole()}</p>`;
+        roleSpecificInfo = `<li class="list-group-item">GitHub: <a href="https://github.com/${employee.github}" target="_blank">${employee.github}</a></li>`;
+    };
     if (employee.getRole() === "Intern") {
-        return `
-        <div class="card col-12 col-md-6 col-lg-4 m-3 px-0 shadow-lg" style="max-width: 18rem;">
-            <div class="card-header text-white bg-primary">
-                <p>${employee.name}</p>
-                <p><i class="fa-solid fa-user-graduate"></i> ${employee.getRole()}</p>
-            </div>
-            <div class="card-body">
-                <ul class="list-group list-group-flush">
-                    <li class="list-group-item">ID: ${employee.id}</li>
-                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                    <li class="list-group-item">School: ${employee.school}</li>
+        cardOutput += `
+            <p class="fs-5"><i class="fa-solid fa-user-graduate"></i> ${employee.getRole()}</p>`;
+        roleSpecificInfo = `<li class="list-group-item">School: ${employee.school}</li>`;
+    };
+    cardOutput += `
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group list-group-flush border my-2">
+                <li class="list-group-item">ID: ${employee.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                ` + roleSpecificInfo + `
                 </ul>
             </div>
-        </div>`
-    }
-}
+        </div>`;
+    return cardOutput;
+};
 
 const generateTeamCards = teamArr => {
     let teamHtml = ``;
@@ -57,7 +38,7 @@ const generateTeamCards = teamArr => {
         teamHtml += generateCard(element);
     })
     return teamHtml;
-} 
+}
 
 module.exports = templateDataArr => {
     return `
@@ -72,14 +53,14 @@ module.exports = templateDataArr => {
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>Team Profile</title>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css">
     </head>
         
     <body>
         <header>
-            <div class="jumbotron jumbotron-fluid">
+            <div class="container-fluid bg-danger p-5">
                 <div class="container">
-                    <h1 class="display-1 font-weight-bold">My Team</h1>
+                    <h1 class="display-1 font-weight-bold text-light">My Team</h1>
                 </div>
             </div>
         </header>
