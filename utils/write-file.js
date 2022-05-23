230fe48f6b80aa6dde2338c69f7769e56ff704c0
Will G <wgrout87@gmail.com>
2022-05-22 19:45:53 -0600

    added writeFile ability to check for dist folder

--- utils/write-file.js
+++ utils/write-file.js
@@ -1,6 +1,11 @@
 const fs = require("fs");
 
 const writeFile = fileContent => {
+    if (!fs.existsSync("./dist")) {
+        fs.mkdir("./dist", (err) => {
+            if (err) throw err;
+        });
+    };
     return new Promise((resolve, reject) => {
         fs.writeFile("./dist/team-profile.html", fileContent, err => {
             if (err) {
